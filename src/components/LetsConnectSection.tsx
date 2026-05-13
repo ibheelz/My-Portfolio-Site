bce4f020bbc6ec25b9bcdd739f313c5de6adad8f
ibheelz <ibhxxlz@gmail.com>
2026-05-13 12:47:23 +0100

    Make image height dynamic to match text content height

--- src/components/LetsConnectSection.tsx
+++ src/components/LetsConnectSection.tsx
@@ -12,7 +12,7 @@ export default function LetsConnectSection() {
       {/* Outer container - flex row */}
       <div className="flex flex-col lg:flex-row gap-8 items-stretch lg:items-stretch" style={{ gap: '32px' }}>
         {/* Left Side - Photograph */}
-        <div className="w-full lg:w-1/2 relative flex-shrink-0" style={{ height: '440px' }}>
+        <div className="w-full lg:w-1/2 relative flex-shrink-0">
           <div className="relative w-full h-full rounded-2xl overflow-visible" style={{ borderRadius: '16px' }}>
             <Image
               src="https://framerusercontent.com/images/5ylj0gysRWJbYU3eEdJqMvks02w.png"
@@ -36,9 +36,9 @@ export default function LetsConnectSection() {
         </div>
 
         {/* Right Side - Text and Contact Area */}
-        <div className="flex-1 w-full lg:w-1/2 flex flex-col overflow-visible" style={{ height: '440px' }}>
+        <div className="flex-1 w-full lg:w-1/2 flex flex-col overflow-visible">
           {/* Main content wrapper with max-width */}
-          <div className="flex flex-col gap-6 max-w-[640px] mx-auto lg:mx-0 w-full justify-between h-full" style={{ gap: '24px' }}>
+          <div className="flex flex-col gap-6 max-w-[640px] mx-auto lg:mx-0 w-full justify-between" style={{ gap: '24px' }}>
 
             {/* Top Text Group */}
             <div className="flex flex-col gap-2.5" style={{ gap: '10px' }}>
